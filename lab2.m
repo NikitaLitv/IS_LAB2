caa4788c11b3a46249e425276d32d21b1ea5b7e1
NikitaLitv <nikita.litvinenko@stud.vilniustech.lt>
2024-11-14 19:22:09 +0200

    Update lab2.m

--- lab2.m
+++ lab2.m
@@ -4,9 +4,12 @@ close all
 
 %Turimi duomenys
  x = 0.1:1/22:1;
+ x_test=0.1:1/200:1;
  y = ((1+0.6*sin(2*pi*x/0.7))+0.3*sin(2*pi*x))/2;
+ y_test = ((1+0.6*sin(2*pi*x_test/0.7))+0.3*sin(2*pi*x_test))/2;
+
 figure(1)
-plot(x,y)
+plot(x_test,y_test)
 grid on
 
 %Svoriai
@@ -37,6 +40,8 @@ b1_2=rand(1);
 eta=0.1;
 
 Y=zeros(1,length(x));
+
+Y_test=zeros(1,length(x));
 
 ciklu_sk=10000;
 
@@ -96,7 +101,35 @@ for j=1:ciklu_sk
     end
 end
 
+%Testing function
+for i=1:length(x_test)
+        v1_1=w11_1*x_test(i)+b1_1;
+        v2_1=w21_1*x_test(i)+b2_1;
+        v3_1=w31_1*x_test(i)+b3_1;
+        v4_1=w41_1*x_test(i)+b4_1;
+        v5_1=w51_1*x_test(i)+b5_1;
+        v6_1=w61_1*x_test(i)+b6_1;
+        %aktyvacijos funkcija
+        y1_1=tanh(v1_1);
+        y2_1=tanh(v2_1);
+        y3_1=tanh(v3_1);
+        y4_1=tanh(v4_1);
+        y5_1=tanh(v5_1);
+        y6_1=tanh(v6_1);
+        %Antras
+        v1_2=y1_1*w11_2+y2_1*w12_2+y3_1*w13_2+y4_1*w14_2+y5_1*w15_2+y6_1*w16_2+b1_2;
+        %Išėjimo sluoksnio aktyvacija
+        y1_2=v1_2;
+        Y_test(i)=y1_2;
+ end
+
+%plot testing results
 hold on
-plot(x,Y)
+plot(x_test,Y_test)
 hold off
 legend('Target', 'Predicted')
+
+%hold on
+%plot(x_test,Y_test)
+%hold off
+%legend('Target', 'Predicted')
