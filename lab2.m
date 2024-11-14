0482ee380ac4728136b59f046cf4e9731d811f1e
NikitaLitv <nikita.litvinenko@stud.vilniustech.lt>
2024-11-14 16:58:19 +0200

    Add files via upload

---
+++ lab2.m
@@ -0,0 +1,102 @@
+%% lab2
+clc
+close all
+
+%Turimi duomenys
+ x = 0.1:1/22:1;
+ y = ((1+0.6*sin(2*pi*x/0.7))+0.3*sin(2*pi*x))/2;
+figure(1)
+plot(x,y)
+grid on
+
+%Svoriai
+%Pirmas sluoksnis
+w11_1=rand(1);
+w21_1=rand(1);
+w31_1=rand(1);
+w41_1=rand(1);
+w51_1=rand(1);
+w61_1=rand(1);
+
+b1_1=rand(1);
+b2_1=rand(1);
+b3_1=rand(1);
+b4_1=rand(1);
+b5_1=rand(1);
+b6_1=rand(1);
+%Antras sluoksnis
+w11_2=rand(1);
+w12_2=rand(1);
+w13_2=rand(1);
+w14_2=rand(1);
+w15_2=rand(1);
+w16_2=rand(1);
+
+b1_2=rand(1);
+
+eta=0.1;
+
+Y=zeros(1,length(x));
+
+ciklu_sk=10000;
+
+%Atsakas
+for j=1:ciklu_sk
+    for i=1:length(x)
+        v1_1=w11_1*x(i)+b1_1;
+        v2_1=w21_1*x(i)+b2_1;
+        v3_1=w31_1*x(i)+b3_1;
+        v4_1=w41_1*x(i)+b4_1;
+        v5_1=w51_1*x(i)+b5_1;
+        v6_1=w61_1*x(i)+b6_1;
+        %aktyvacijos funkcija
+        y1_1=tanh(v1_1);
+        y2_1=tanh(v2_1);
+        y3_1=tanh(v3_1);
+        y4_1=tanh(v4_1);
+        y5_1=tanh(v5_1);
+        y6_1=tanh(v6_1);
+        %Antras
+        v1_2=y1_1*w11_2+y2_1*w12_2+y3_1*w13_2+y4_1*w14_2+y5_1*w15_2+y6_1*w16_2+b1_2;
+        %Išėjimo sluoksnio aktyvacija
+        y1_2=v1_2;
+        Y(i)=y1_2;
+        %Klaidos skaiciavimas
+        e=y(i)-y1_2;
+        %Svoriu atnaujinimas
+        delta1_2=e;
+        %Pasleptojo sl klaidos gradientas
+        delta1_1=(1-tanh(v1_1)^2)*delta1_2*w11_2;
+        delta2_1=(1-tanh(v2_1)^2)*delta1_2*w12_2;
+        delta3_1=(1-tanh(v3_1)^2)*delta1_2*w13_2;
+        delta4_1=(1-tanh(v4_1)^2)*delta1_2*w14_2;
+        delta5_1=(1-tanh(v5_1)^2)*delta1_2*w15_2;
+        delta6_1=(1-tanh(v6_1)^2)*delta1_2*w16_2;
+        %Atnaujinam svorius
+        w11_2=w11_2+eta*delta1_2*y1_1;
+        w12_2=w12_2+eta*delta1_2*y2_1;
+        w13_2=w13_2+eta*delta1_2*y3_1;
+        w14_2=w14_2+eta*delta1_2*y4_1;
+        w15_2=w15_2+eta*delta1_2*y5_1;
+        w16_2=w16_2+eta*delta1_2*y6_1;
+        b1_2=b1_2+eta*delta1_2;
+        %pasleptasis
+        w11_1=w11_1+eta*delta1_1*x(i);
+        w21_1=w21_1+eta*delta2_1*x(i);
+        w31_1=w31_1+eta*delta3_1*x(i);
+        w41_1=w41_1+eta*delta4_1*x(i);
+        w51_1=w51_1+eta*delta5_1*x(i);
+        w61_1=w61_1+eta*delta6_1*x(i);
+        b1_1=b1_1+eta*delta1_1;
+        b2_1=b2_1+eta*delta2_1;
+        b3_1=b3_1+eta*delta3_1;
+        b4_1=b4_1+eta*delta4_1;
+        b5_1=b5_1+eta*delta5_1;
+        b6_1=b6_1+eta*delta6_1;
+    end
+end
+
+hold on
+plot(x,Y)
+hold off
+legend('Target', 'Predicted')
---
+++ lab2_surface_approx.m
@@ -0,0 +1,143 @@
+%% lab2
+clc
+close all
+
+%Turimi duomenys
+[x1, x2] = meshgrid(0:0.05:1, 0:0.05:1);%create twoD grid to solve surface approx
+y = (1 + 0.6 * sin(2 * pi * x1 / 0.7)) + 0.3 * sin(2 * pi * x2);  % modifikuota funkcija, turinti du kintamuosius
+
+
+%Svoriai
+%Pirmas sluoksnis
+w11_1=rand(1);
+w21_1=rand(1);
+w31_1=rand(1);
+w41_1=rand(1);
+w51_1=rand(1);
+w61_1=rand(1);
+%Antras iejimas
+w12_1=rand(1);
+w22_1=rand(1);
+w32_1=rand(1);
+w42_1=rand(1);
+w52_1=rand(1);
+w62_1=rand(1);
+
+
+b1_1=rand(1);
+b2_1=rand(1);
+b3_1=rand(1);
+b4_1=rand(1);
+b5_1=rand(1);
+b6_1=rand(1);
+
+%Antras sluoksnis
+w11_2=rand(1);
+w12_2=rand(1);
+w13_2=rand(1);
+w14_2=rand(1);
+w15_2=rand(1);
+w16_2=rand(1);
+
+b1_2=rand(1);
+
+eta=0.1;
+
+% Initialize output matrix Y to match the dimensions of x1 and x2
+Y = zeros(size(x1));%we need matrix same size as x1 and x2 since we work with 2D
+
+% Number of epochs
+ciklu_sk = 99998;
+
+% Training loop
+for j = 1:ciklu_sk
+    for i = 1:size(x1, 1)%iterate through all x1 values
+        for k = 1:size(x1, 2)%iterate through all x2 values
+            % Forward pass for each element in x1, x2
+            v1_1 = w11_1 * x1(i, k) + w12_1 * x2(i, k) + b1_1;
+            v2_1 = w21_1 * x1(i, k) + w22_1 * x2(i, k) + b2_1;
+            v3_1 = w31_1 * x1(i, k) + w32_1 * x2(i, k) + b3_1;
+            v4_1 = w41_1 * x1(i, k) + w42_1 * x2(i, k) + b4_1;
+            v5_1 = w51_1 * x1(i, k) + w52_1 * x2(i, k) + b5_1;
+            v6_1 = w61_1 * x1(i, k) + w62_1 * x2(i, k) + b6_1;
+
+            % Activation for hidden layer
+            y1_1 = tanh(v1_1);
+            y2_1 = tanh(v2_1);
+            y3_1 = tanh(v3_1);
+            y4_1 = tanh(v4_1);
+            y5_1 = tanh(v5_1);
+            y6_1 = tanh(v6_1);
+
+            % Output neuron
+            v1_2 = y1_1 * w11_2 + y2_1 * w12_2 + y3_1 * w13_2 + ...
+                   y4_1 * w14_2 + y5_1 * w15_2 + y6_1 * w16_2 + b1_2;
+
+            % Output layer activation (linear)
+            y1_2 = v1_2;
+
+            % Store the output in the matrix Y
+            Y(i, k) = y1_2;
+
+            % Compute the error
+            e = y(i, k) - y1_2;
+
+            % Backpropagation
+            delta1_2 = e;
+
+            % Hidden layer error gradients
+            delta1_1 = (1 - tanh(v1_1)^2) * delta1_2 * w11_2;
+            delta2_1 = (1 - tanh(v2_1)^2) * delta1_2 * w12_2;
+            delta3_1 = (1 - tanh(v3_1)^2) * delta1_2 * w13_2;
+            delta4_1 = (1 - tanh(v4_1)^2) * delta1_2 * w14_2;
+            delta5_1 = (1 - tanh(v5_1)^2) * delta1_2 * w15_2;
+            delta6_1 = (1 - tanh(v6_1)^2) * delta1_2 * w16_2;
+
+            % Update weights for output layer
+            w11_2 = w11_2 + eta * delta1_2 * y1_1;
+            w12_2 = w12_2 + eta * delta1_2 * y2_1;
+            w13_2 = w13_2 + eta * delta1_2 * y3_1;
+            w14_2 = w14_2 + eta * delta1_2 * y4_1;
+            w15_2 = w15_2 + eta * delta1_2 * y5_1;
+            w16_2 = w16_2 + eta * delta1_2 * y6_1;
+            b1_2 = b1_2 + eta * delta1_2;
+
+            % Update weights for hidden layer
+            w11_1 = w11_1 + eta * delta1_1 * x1(i, k);
+            w12_1 = w12_1 + eta * delta1_1 * x2(i, k);
+            w21_1 = w21_1 + eta * delta2_1 * x1(i, k);
+            w22_1 = w22_1 + eta * delta2_1 * x2(i, k);
+            w31_1 = w31_1 + eta * delta3_1 * x1(i, k);
+            w32_1 = w32_1 + eta * delta3_1 * x2(i, k);
+            w41_1 = w41_1 + eta * delta4_1 * x1(i, k);
+            w42_1 = w42_1 + eta * delta4_1 * x2(i, k);
+            w51_1 = w51_1 + eta * delta5_1 * x1(i, k);
+            w52_1 = w52_1 + eta * delta5_1 * x2(i, k);
+            w61_1 = w61_1 + eta * delta6_1 * x1(i, k);
+            w62_1 = w62_1 + eta * delta6_1 * x2(i, k);
+
+            b1_1 = b1_1 + eta * delta1_1;
+            b2_1 = b2_1 + eta * delta2_1;
+            b3_1 = b3_1 + eta * delta3_1;
+            b4_1 = b4_1 + eta * delta4_1;
+            b5_1 = b5_1 + eta * delta5_1;
+            b6_1 = b6_1 + eta * delta6_1;
+        end
+    end
+end
+
+% Plot the approximated surface with a specific color (e.g., red)
+figure;
+surf(x1, x2, Y, 'FaceColor', 'red', 'EdgeColor', 'green');  % Approximated surface
+hold on;
+
+% Plot the target surface with a different color (e.g., blue)
+surf(x1, x2, y, 'FaceColor', 'blue', 'EdgeColor', 'green');  % Target surface
+
+% Add a legend and labels
+legend('Predicted Surface', 'Target Surface');
+xlabel('x1');
+ylabel('x2');
+zlabel('Output');
+title('Surface Approximation Comparison');
+hold off;
